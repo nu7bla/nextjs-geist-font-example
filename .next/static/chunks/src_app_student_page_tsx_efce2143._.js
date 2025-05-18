20934e6c94073a839f3026c10dc46de619999b6c
b1nd <blnddiphone@gmail.com>
2025-05-18 15:41:20 -0700

    Add .next/static/chunks/src_app_student_page_tsx_efce2143._.js 1747608079141

---
+++ .next/static/chunks/src_app_student_page_tsx_efce2143._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_41c7047e._.js",
+  "static/chunks/src_ee6f7ebd._.js"
+],
+    source: "dynamic"
+});
